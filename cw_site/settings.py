282f302db24583aa533bde1a910ad96f19f55291
yuuta3594 <yuuta3594@outlook.jp>
2020-02-02 21:18:07 +0900

    ok

--- cw_site/settings.py
+++ cw_site/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'cw_site.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'django',
-    #     'USER': 'django',
-    #     'PASSWORD': 'django',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
 
 # Password validation
